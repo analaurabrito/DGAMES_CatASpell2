57f9369c4d9c5609788da39abd957c59dcf7e6c9
Anna Maria Lima <87787347+Annmochii@users.noreply.github.com>
2024-06-17 21:39:26 -0300

    manual merge /assets

---
+++ Cat a Spell/Assets/Scripts/PlatformController.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlatformController : MonoBehaviour
+{
+    public PlayerController Player;
+    public GameObject LevelCompleteText;
+    public GameObject NextLevelButton;
+    public GameObject RestartLevelButton;
+    public GameObject RestartGameButton;
+    public GameObject DropBoneText;
+    public GameObject BoneOnPlayer;
+    public GameObject BoneOnPlatform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        BoneOnPlatform.SetActive(false);
+        DropBoneText.SetActive(false);
+        LevelCompleteText.SetActive(false);
+        NextLevelButton.SetActive(false);
+        RestartGameButton.SetActive(false);
+        RestartLevelButton.SetActive(false);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+
+        if (other.gameObject.tag == "Player")
+        {
+            if (BoneOnPlayer.activeSelf == true)
+            {
+                DropBoneText.SetActive(true);
+            }
+            if (Input.GetKey(KeyCode.Q))
+            {
+                BoneOnPlatform.SetActive(true);
+                BoneOnPlayer.SetActive(false);
+                DropBoneText.SetActive(false);
+            }
+
+            if (BoneOnPlatform.activeSelf == true)
+            {
+                DropBoneText.SetActive(false);
+
+                LevelCompleteText.SetActive(true);
+                NextLevelButton.SetActive(true);
+                RestartLevelButton.SetActive(true);
+                RestartGameButton.SetActive(true);
+
+                Player.moveSpeed = 0;
+            }
+        }
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        DropBoneText.SetActive(false);
+    }
+
+    public void ResetTheLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        print("Ta funcionando.");
+    }
+}
